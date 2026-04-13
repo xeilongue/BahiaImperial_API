22852d0792cf2d9472b67987a2689ce191791757
João Victor <jvencarnacao05@gmail.com>
2026-04-13 11:29:43 -0300

    raw: Adiciona dependencias de api e conexao

---
+++ BahiaImperial_API/Models/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace BahiaImperial_API.Models
+{
+    public class Clients
+    {
+        public string CpfCnpj { get; set; }
+        public string Name { get; set; }
+        public decimal MonthlyIncome { get; set; }
+    }
+}
